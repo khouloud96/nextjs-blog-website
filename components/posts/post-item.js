ac46b415253fbad3d66f617e615c2c7be2be25ae
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-09-16 14:56:51 +0200

    adding All posts page + Post Details page

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -12,11 +12,18 @@ function PostItem (props) {
     });
 
     const imagePath = `/images/posts/${slug}/${image}`;
+    const linkPath = `/posts/${slug}`;
 
     return <li className={classes.post}>
-        <Link>
+        <Link href={linkPath}>
             <div className={classes.image}>
-                <Image src={imagePath} alt={title} width={300} height={300} />
+                <Image 
+                    src={imagePath} 
+                    alt={title} 
+                    width={300} 
+                    height={300} 
+                    layout="responsive" 
+                />
             </div>
             <div className={classes.content}>
                 <h3>{title}</h3>

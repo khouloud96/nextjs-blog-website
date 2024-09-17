4bbba3436f1c24263ab09e3c79c980bd44b6e93c
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-09-17 18:09:24 +0200

    adding head data & add _document.js file & use React Portals

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,19 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
     return (
         <Fragment>
-            <Hero></Hero>
+            <Head>
+                <title>Khouloud's blog</title>
+                <meta
+                name='description'
+                content='I post about programming and web development.'
+                />
+            </Head>
+            <Hero />
             <FeaturedPosts posts={props.posts} />
         </Fragment>
     );

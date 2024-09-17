d00eeccd85f774bfdbcf64dbb819613ca4a1087c
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-09-17 13:37:38 +0200

    sending data from the client to the API Route & storing messages in Mongodb database

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,63 @@
+import { useState } from 'react';
 import classes from './contact-form.module.css';
 
 function ContactForm() {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredMessage, setEnteredMessage] = useState('');
+
+    function sendMessageHandler(event) {
+        event.preventDefault();
+
+        //optional: add client-side validation
+
+        fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: enteredEmail,
+                name: enteredName,
+                message: enteredMessage,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+
     return(
-        <section className={classes.contact}>
+        <section className={classes.contact} onSubmit={sendMessageHandler}>
             <h1>How can I help you?</h1>
             <form className={classes.form}>
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor='email'>Your Email</label>
-                        <input type='email' id='email' required />
+                        <input 
+                            type='email' 
+                            id='email' 
+                            required
+                            value={enteredEmail}
+                            onChange={(event) => setEnteredEmail(event.target.value)} 
+                        />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor='name'>Your Name</label>
-                        <input type='text' id='name' required />
+                        <input 
+                            type='text' 
+                            id='name' 
+                            required 
+                            value={enteredName}
+                            onChange={(event) => setEnteredName(event.target.value)} 
+                        />
                     </div>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='message'>Your Message</label>
-                    <textarea id='message' rows='5' />
+                    <textarea 
+                        id='message' 
+                        rows='5'
+                        value={enteredMessage}
+                        onChange={(event) => setEnteredMessage(event.target.value)}  
+                    />
                 </div>
                 <div className={classes.actions}>
                     <button>Send Message</button>
---
+++ pages/api/contact.js
@@ -0,0 +1,53 @@
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+    if(req.method === 'POST') {
+        const { email, name, message } = req.body;
+
+        // validate form inputs on server-side
+        if (
+            !email ||
+            !email.includes('@') || 
+            !name ||
+            name.trim() === '' ||
+            !message ||
+            message.trim() === ''
+        ) {
+            res.status(422).json({message: 'Invalid input.'});
+            return;
+        }
+
+        //store data in database
+        const newMessage = {
+            email,
+            name,
+            message
+        };
+
+        let client;
+        try{
+        client = await MongoClient.connect(
+            'mongodb+srv://ademmiss:zTtk5QpYQGiEs79p@cluster0.zhbg3.mongodb.net/my-site?retryWrites=true&w=majority&appName=Cluster0'
+          );
+        } catch(error) {
+            res.status(500).json({message: 'Could not connect to Database'});
+            return;
+        }
+        const db = client.db();
+
+        try{
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch(error) {
+            client.close();
+            res.status(500).json({message: 'Failed to store message'});
+            return;
+        }
+
+        client.close();
+        res.status(201).json({message: 'Success stored message!', message: newMessage});
+
+    }
+}
+
+export default handler;
